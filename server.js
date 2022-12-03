047f8fa5bc98bc7e6b21421fbd4463a14fc5ed83
Andressavilela <andressavilela4@hotmail.com>
2022-12-03 14:58:21 -0300

    segundo commit

---
+++ server.js
@@ -0,0 +1,4 @@
+const app = require("./src/app.js")
+
+app.listen(1010, () => console.log("Rodando servidor - Projeto Final!"))
+
---
+++ src/app.js
@@ -0,0 +1,15 @@
+require("dotenv-safe").config()
+const express = require ("express")
+const cors = require("cors")
+const database = require("./config/database")
+const indexRouter = require("./routes/indexRoutes.js")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+database.connect()
+
+app.use(indexRouter)
+
+module.exports = app
